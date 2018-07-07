4af18c2647c47ce85377bd86252a6a0778290a73
Manoj Kumar Maharana <manoj@triveous.com>
2018-07-07 22:06:41 +0530

    Add: schema to middleware

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,10 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql')
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql')
+const _ = require('lodash')
+
+const users = [
+  {id: '23', firstName: 'Manoj', age: 23},
+  {id: '2324', firstName: 'Kanhu', age: 23}
+]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -9,15 +15,19 @@ const UserType = new GraphQLObjectType({
   }
 })
 
-const RootQueryType = new GraphQLObjectType({
-  name: 'RootQuery',
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
   fields: {
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        
+        return _.find(users, {id: args.id})
       }
     }
   }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
